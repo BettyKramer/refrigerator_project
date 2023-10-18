13f2b8009f44f4bdc804ed2e4e18fdf361b4fa09
bettyKramer <bettykr2003@gmail.com>
2023-10-18 16:21:26 +0300

    made tostring functions

--- refrigerator/refrigerator/Item.cs
+++ refrigerator/refrigerator/Item.cs
@@ -1,9 +1,23 @@
 ﻿namespace refrigerator
 {
-    internal class Item
+    class Item
     {
-        public string name;
-        public int id {  get; }    
-        enum typeOfItme;
+        public string itemName { get; }
+        public int itemId { get; }
+        foodOrDrnik fd ;
+        kashroot kasher;
+        public DateTime expiryDate { get; }
+        public int size { get; }
+
+        public void toString()
+        {
+            Console.WriteLine("item name: "+itemName+" item id: "+itemId+" type: "+fd+" kasher: "+kasher+
+                " expiry date: "+expiryDate+" size: "+size);
+        }
+
+        enum foodOrDrnik { FOOD, DRINK }
+        enum kashroot { MEAT, MMILK ,PARVE}
     }
+
 }
+
